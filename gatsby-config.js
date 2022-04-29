1eb78e4f1ed723209ee14d2318d377b4a4a958dd
ghilas <1779458@crosemont.qc.ca>
2022-04-28 23:32:26 -0400

    netlify config settings file

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,8 +10,7 @@ module.exports = {
       resolve: `gatsby-source-strapi`,
       options: {
         apiURL: process.env.DEPLOY_URL 
-        ? 'https://strapi-gatsb-pg-restaurant.herokuapp.com' 
-        : `http://localhost:1337`,
+        ? 'https://strapi-gatsb-pg-restaurant.herokuapp.com' : `http://localhost:1337`,
         queryLimit: 1000, // Defaults to 100
         collectionTypes: [`item`],
       },

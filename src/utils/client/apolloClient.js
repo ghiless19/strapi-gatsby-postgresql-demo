02e9c6670a747c7d93431bb4eb307e64afbd2e25
ghilas <1779458@crosemont.qc.ca>
2022-04-29 19:24:44 -0400

    graphql config

--- src/utils/client/apolloClient.js
+++ src/utils/client/apolloClient.js
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, HttpLink } from "@apollo/client";
 import fetch from 'isomorphic-fetch';
 const httplink = new HttpLink({
-  uri: `http://localhost:1337/graphql`,
+  uri: process.env.DEPLOY_URL 
+  ? 'https://strapi-gatsb-pg-restaurant.herokuapp.com/graphql' : `http://localhost:1337/graphql`,
   fetch
 });
 const client = new ApolloClient({
